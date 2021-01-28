7cb88af0b35024e848ce8dbc44a4f7a5dcf4df66
olivertemple <70803502+olivertemple@users.noreply.github.com>
2021-01-28 12:49:27 +0000

    weather finnished with location and wave data for westward ho!

--- tide.js
+++ tide.js
@@ -4,7 +4,7 @@ function getTideData(){
     req.onload = function(){
         if (req.status >= 200 && req.status < 400) {
             tideData = (req.response);
-            document.getElementById("tides").innerHTML=(tideData)
+            document.getElementById("tides").innerHTML = (tideData)
         }else{
             console.log("error")
         }
@@ -14,3 +14,51 @@ function getTideData(){
 
 
 getTideData();
+
+
+function getWaveData(){
+    waveReq = new XMLHttpRequest();
+    waveReq.open("GET","./waveHeight.txt",true);
+    waveReq.onload = function(){
+        if (waveReq.status >=200 && waveReq.status < 400){
+            waveData = waveReq.response.split(",");
+            waveData.pop()
+            showWaveData(waveData)
+            console.log(waveData)
+        }else{
+            console.log("error")
+        }
+    }
+    waveReq.send();
+}
+getWaveData();
+
+
+function showWaveData(data){
+    dayNum = 0
+    var today = new Date()
+    for (let i=0; i<data.length; i++){
+        if(i%8==0){
+            var dayList = ["Sun","Mon","Tue","Wed","Thurs","Fri","Sat"]
+            date = new Date(today);
+            date.setDate(date.getDate() + dayNum);
+            console.log(date.getDate())
+            var div= document.createElement("div1");
+            var h1 = document.createElement("h1");
+            var dateHeading = document.createElement("h1")
+            dateHeading.setAttribute("id","date")
+            dateHeading.appendChild(document.createTextNode(dayList[date.getDay()]+" "+date.getDate()));
+            console.log(dateHeading);
+
+            h1.appendChild(document.createTextNode(String(data[i])+"m"));
+            div.appendChild(dateHeading)
+            div.appendChild(h1)
+            dayNum += 1
+        }else{
+            var h1 = document.createElement("h1");
+            h1.appendChild(document.createTextNode(String(data[i])+"m"));
+            div.appendChild(h1);
+        };
+        document.getElementById("waves").appendChild(div)
+    }
+}
