961c41a1b0dcbe953fb7bc37bdf82d0e149c7d33
olivertemple <70803502+olivertemple@users.noreply.github.com>
2021-02-11 11:52:56 +0000

    desktop styling done+improved offline support

--- getTideData.py
+++ getTideData.py
@@ -4,11 +4,12 @@ from bs4 import BeautifulSoup as bs
 req = requests.get("https://www.tidetime.org/europe/united-kingdom/westward-ho.htm")
 print(req)
 soup = bs(req.text)
+print(req.text)
 file = open("tideTable.txt", "w")
 file.write(str(soup.find(id="tideTable")))
 file.close()
 
-
+'''
 waveReq = requests.get("https://www.windfinder.com/forecast/westward_ho")
 print(waveReq)
 soup1 = bs(waveReq.text)
@@ -24,3 +25,4 @@ for item in spans:
     waveFile.write(item[0].text+",")
 
 waveFile.close()
+'''
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,24 +1,38 @@
-/*const mem = "weather";
+const mem = "weather";
 const assets = [
   "./",
   "./index.html",
   "./sail.html",
-  "./resources/",
-  "./canvasjs.min.js"
+  "./resources/arrow-dark.png",
+  "./resources/arrow-light.png",
+  "./resources/location.png",
+  "./resources/maskable_icon_x1.png",
+  "./resources/maskable_icon_x96.png",
+  "./resources/maskable_icon_x128.png",
+  "./resources/maskable_icon_x180.png",
+  "./resources/maskable_icon_x192.png",
+  "./resources/sailboat-dark.png",
+  "./resources/sailboat-light.png",
+  "./resources/sunrise.png",
+  "./resources/sunset.png",
+  "./resources/weather-dark.png",
+  "./resources/weather-light.png",
+  "./resources/wind.png",
+  "./canvasjs.min.js",
+  "./manifest.json"
 ];
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(mem).then(cache => {
-      cache.addAll(assets);
-    })
-  );
-});
 
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(mem).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.addAll(assets);
+          return response;
+        });
+      });
     })
   );
 });
